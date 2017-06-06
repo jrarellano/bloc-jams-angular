743c892627252b93578499db3c572837a5a107c6
juanita arellano <jrarellano1980@yahoo.com>
2017-06-05 22:56:24 -0400

    assignment-3-Routing-and-States:completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,29 @@
 (function() {
-     function config($stateProvider, $locationProvider) {
-       $locationProvider
-        .html5Mode({
-            enabled: true,
-            requireBase: false
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
         });
+
         $stateProvider
          .state('landing', {
              url: '/',
              templateUrl: '/templates/landing.html'
-           })
-           .state('album', {
-               url: '/album',
-               templateUrl: '/templates/album.html'
-           });         
+         })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         })
+//        assignment
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+        });
+//        end of assignment
+    }
 
-     }
-
-     angular
-              .module('blocJams', ['ui.router'])
-              .config(config);
-      })();
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
